1cf2f7b7a5b08389184aa873f72e38aa2bba45e3
FarhanKurnia <itsfarhankurnia@gmail.com>
2022-03-20 10:48:52 +0700

    fixing seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            //ReviewSeeder::class,
+            ReviewSeeder::class,
             DestinasiSeeder::class,
             //UserSeeder::class,
         ]);
